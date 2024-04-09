d00bf3381e9b37bc308226289a8d31b7283f165d
Will Duncan <willduncan@Wills-MacBook-Pro-63.local>
2024-04-09 19:21:09 -0400

    history service

---
+++ src/main/java/edu/iu/wjduncan/historyservice/rabbitmq/MQReceiver.java
@@ -0,0 +1,24 @@
+package edu.iu.wjduncan.historyservice.rabbitmq;
+
+import com.google.gson.Gson;
+import edu.iu.wjduncan.historyservice.model.PrimesRecord;
+import edu.iu.wjduncan.historyservice.repository.PrimesHistoryRepository;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MQReceiver {
+    private final PrimesHistoryRepository primesHistoryRepository;
+
+    public MQReceiver(PrimesHistoryRepository primesHistoryRepository){
+        this.primesHistoryRepository = primesHistoryRepository;
+    }
+    @RabbitListener(queues = {"primes"})
+    public void receiveMessage(@Payload String message){
+        System.out.println(message);
+        Gson gson = new Gson();
+        PrimesRecord primesRecord = gson.fromJson(message, PrimesRecord.class);
+        primesHistoryRepository.save(primesRecord);
+    }
+}
